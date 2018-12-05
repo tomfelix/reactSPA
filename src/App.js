2d41c21fd263e03ccaa040a9f6c4b2825cbba74d
TomFelix <tomasz.agacinski@gmail.com>
2018-12-05 23:41:09 +0100

    finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from 'react-router-dom';
+
+import About from './pages/about';
+import Products from './pages/products';
+import Contact from './pages/contact';
+import Home from './pages/home';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <HashRouter>
+        <div>
+          <h1 className="title">SPA</h1>
+          <ul className="header">
+            <li><NavLink className="nav-link" to="/">Home</NavLink></li>
+            <li><NavLink className="nav-link" to="/about">About</NavLink></li>
+            <li><NavLink className="nav-link" to="/products">Products</NavLink></li>
+            <li><NavLink className="nav-link" to="/contact">Contact</NavLink></li>
+          </ul>
+          <div className="content">
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/products" component={Products} />
+            <Route path="/contact" component={Contact} />
+          </div>
+        </div>
+      </HashRouter>
     );
   }
 }
